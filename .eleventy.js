c3ffce0561a7466e092a9a90faa4259ec468b8d8
John Nolan <john.nolan@methods.co.uk>
2023-08-07 15:13:57 +0100

    Add mermaid eleventy plugin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const dateFilter = require('./src/filters/date-filter.js');
 const date24HourFilter = require('./src/filters/date24Hours-filter.js');
+const pluginMermaid = require('@kevingimbel/eleventy-plugin-mermaid');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/_includes/css": "assets" });
@@ -16,16 +17,17 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(inclusiveLangPlugin);
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginMermaid);
 
   eleventyConfig.addFilter("log", (value) => {
     console.log(value);
   });
-  eleventyConfig.addFilter("limit", function(array, limit) {
+  eleventyConfig.addFilter("limit", function (array, limit) {
     return array.slice(0, limit);
   });
   eleventyConfig.addFilter('dateFilter', dateFilter);
   eleventyConfig.addFilter('date24HourFilter', date24HourFilter);
-  
+
   return {
     dir: { input: "src", output: "_site", data: "_data" },
     templateFormats: ["njk", "md", "css", "html", "yml"],
